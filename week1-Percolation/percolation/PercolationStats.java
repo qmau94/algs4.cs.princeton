8e1f0ad634f38483c07d57c6ecf15ba42a2abaa5
qmau94 <qmau94@gmail.com>
2018-02-20 19:11:35 +0900

    week1-Percolation

---
+++ week1-Percolation/percolation/PercolationStats.java
@@ -0,0 +1,64 @@
+package algorithms.week1.percolation;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private double[] threshold;
+    private final int size;
+    private final int trials;
+    private final double CONFIDENCE_95 = 1.96;
+
+    public PercolationStats(int n, int t) {
+        if (n < 0 || t < 0)
+            throw new IllegalArgumentException("n < 0 || t < 0");
+        threshold = new double[t];
+        size = n;
+        trials = t;
+        for (int i = 0; i < t; i++) {
+            Percolation p = new Percolation(size);
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(size) + 1;
+                int col = StdRandom.uniform(size) + 1;
+                if (!p.isOpen(row, col)) {
+                    p.open(row, col);
+                }
+            }
+            threshold[i] = p.numberOfOpenSites() / (double) (size * size);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(threshold);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(threshold);
+    }
+
+    public double confidenceLo() {
+        return mean() - ((CONFIDENCE_95 * stddev()) / Math.sqrt(trials));
+    }
+
+    public double confidenceHi() {
+        return mean() + ((CONFIDENCE_95 * stddev()) / Math.sqrt(trials));
+    }
+
+    public static void main(String[] args) {
+        try {
+            int size = 200;
+            int trials = 3;
+
+            PercolationStats percolationStats = new PercolationStats(size, trials);
+            StdOut.println("mean                    = " + percolationStats.mean());
+            StdOut.println("stddev                  = " + percolationStats.stddev());
+            StdOut.println("95% confidence interval = " + percolationStats.confidenceLo() + ", "
+                    + percolationStats.confidenceHi());
+        } catch (NumberFormatException e) {
+            StdOut.println("Argument must be an integer");
+            return;
+        }
+    }
+
+}
---
+++ week1-Percolation/submissions/Percolation.java
@@ -0,0 +1,82 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    
+    private final int n;
+    private final WeightedQuickUnionUF uf;
+    private boolean[] grid;
+    private int num;
+    
+    public Percolation(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException("n > 0");
+        num = 0;
+        this.n = n;
+        grid = new boolean[n * n];
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        connectToVirtualSites();
+    }
+    
+    private void connectToVirtualSites() {
+        for (int i = 0; i < n; i++) {
+            uf.union(i, n * n);
+        }
+        for (int i = n * (n - 1); i < n * n; i++) {
+            uf.union(i, n * n + 1);
+        }
+    }
+    
+    private void valid(int row, int col) {
+        if (row < 1 || row > n)
+            throw new IllegalArgumentException("1 < row < n");
+        if (col < 1 || col > n)
+            throw new IllegalArgumentException("1 < col < n");
+    }
+    
+    public void open(int row, int col) {
+        valid(row, col);
+        int i = to1dIndex(row, col);
+        if (grid[i])
+            return;
+        grid[i] = true;
+        num++;
+        // top
+        if (row > 1 && isOpen(row - 1, col))
+            uf.union(i, to1dIndex(row - 1, col));
+        // bot
+        if (row < n && isOpen(row + 1, col))
+            uf.union(i, to1dIndex(row + 1, col));
+        // left
+        if (col > 1 && isOpen(row, col - 1))
+            uf.union(i, to1dIndex(row, col - 1));
+        // right
+        if (col < n && isOpen(row, col + 1))
+            uf.union(i, to1dIndex(row, col + 1));
+    }
+    
+    private int to1dIndex(int row, int col) {
+        valid(row, col);
+        return ((row - 1) * n + col - 1);
+    }
+    
+    public boolean isOpen(int row, int col) {
+        valid(row, col);
+        return grid[to1dIndex(row, col)];
+    }
+    
+    public boolean isFull(int row, int col) {
+        valid(row, col);
+        int i = to1dIndex(row, col);
+        return grid[i] && uf.connected(n * n, i);
+    }
+    
+    public int numberOfOpenSites() {
+        return num;
+    }
+    
+    public boolean percolates() {
+        return uf.connected(n * n, n * n + 1);
+    }
+    
+    
+}
